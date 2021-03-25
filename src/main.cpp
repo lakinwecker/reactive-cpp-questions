30e77c99d5cb43ca87ae1b3a51c7c20cf0373d92
Lakin Wecker <lakin.wecker@gmail.com>
2021-03-24 18:59:26 -0600

    Very minor cleanup, but where we left off for the previous week.

--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,104 @@
 
 #include <cstddef>
 #include <iostream>
+#include <optional>
+#include <variant>
 #include <vivid/vivid.h>
-#include <immer/vector.hpp>
+#include <immer/flex_vector.hpp>
 #include <lager/util.hpp>
 
 using namespace vivid::ansi;
 
-struct Action {
-    char c;
+struct YesNoQuestion {
+    std::string question;
+    std::optional<bool> answer;
+};
+struct ShortAnswerQuestion {
+    std::string question;
+    std::string answer;
+};
+//using Question = std::variant<YesNoQuestion, ShortAnswerQuestion>;
+struct Question {
+    std::string question;
+};
+
+struct Nothing {};
+struct Next {};
+struct Prev {};
+struct Answer {
+    std::string answer;
+};
+using Action = std::variant<Nothing, Next, Prev, Answer>;
+
+struct Questionnaire {
+    Question curQuestion;
+    immer::flex_vector<Question> prevQuestions;
+    immer::flex_vector<Question> nextQuestions;
 };
 
 struct Model {
-    char c;
+    Questionnaire questions;
 };
 
+Questionnaire next(Questionnaire q) {
+    return Questionnaire{
+        q.nextQuestions.front(),
+        q.prevQuestions.push_back(q.curQuestion),
+        q.nextQuestions.drop(1)
+    };
+}
+Questionnaire prev(Questionnaire q) {
+    return Questionnaire{
+        q.prevQuestions.back(),
+        q.prevQuestions.take(q.prevQuestions.size()-1),
+        q.nextQuestions.push_front(q.curQuestion)
+    };
+}
+
 Model init() {
-    return {' '};
+    return {
+        Questionnaire{
+            Question{"Are you old enough to drink?"},
+            {},
+            {{"Do you live in Canada?"}, {"Where do you live?"}}
+        }
+    };
 }
 
-Model update(Model const &m, Action const &a) {
-    return {a.c};
+Model update(Model m, Action a) {
+    std::visit(lager::visitor{
+        [&](Nothing) { },
+        [&](Answer) { },
+        [&](Next) { m.questions = next(m.questions); },
+        [&](Prev) { m.questions = prev(m.questions); },
+    }, a);
+    return m;
 }
 
-void display(Model const &m) {
+void display(Model m) {
     terminal::clear();
-    std::cout << grey100 << "[" << green << m.c << grey100 << "]" << std::endl;
+    for(auto const &q : m.questions.prevQuestions) {
+        std::cout << green << q.question << std::endl;
+    }
+    std::cout << white << m.questions.curQuestion.question << std::endl;
+    for(auto const &q : m.questions.nextQuestions) {
+        std::cout << grey100 << q.question << std::endl;
+    }
+
+    //std::cout << grey100 << "[" << white << m.c << grey100 << "]" << std::endl;
+}
+
+Action fromChar(char c) {
+    if (c == 'n') return Next{};
+    if (c == 'p') return Prev{};
+    return Nothing{};
 }
 
 int main() {
-    Model state;
+    Model state = init();
     while (true) {
         display(state);
-        Action a{terminal::nextCharacter()};
+        Action a = fromChar(terminal::nextCharacter());
         state = update(state, a);
     }
     return EXIT_SUCCESS;

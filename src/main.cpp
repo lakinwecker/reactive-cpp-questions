87d6a2364728093c5f92281a8369e902fc31f1a2
Lakin Wecker <lakin.wecker@gmail.com>
2021-04-07 15:10:55 -0600

    Adding display facilities.

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,51 @@
 #include <vivid/vivid.h>
 #include <immer/flex_vector.hpp>
 #include <lager/util.hpp>
+#include <functional>
 
 using namespace vivid::ansi;
+
+template <typename ContainedT, typename ReturnT>
+std::optional<ReturnT> map(std::optional<ContainedT> const &opt, std::function<ReturnT(ContainedT)> const &mapper) {
+    if (!opt.has_value()) return std::nullopt;
+    return mapper(opt.value());
+}
 
 struct YesNoQuestion {
     std::string question;
     std::optional<bool> answer;
 };
+std::ostream &operator<<(std::ostream &out, YesNoQuestion q) {
+    out << q.question
+        << " ["
+        << map<bool, std::string>(
+                q.answer,
+                [](bool answer) { return answer ? "Y" : "N"; }
+            ).value_or(" ")
+        << "]";
+
+    return out;
+}
+
 struct ShortAnswerQuestion {
     std::string question;
-    std::string answer;
+    std::optional<std::string> answer;
 };
-//using Question = std::variant<YesNoQuestion, ShortAnswerQuestion>;
-struct Question {
-    std::string question;
-};
+std::ostream &operator<<(std::ostream &out, ShortAnswerQuestion q) {
+    out << q.question
+        << " ["
+        << q.answer.value_or(" ")
+        << "]";
+    return out;
+}
+
+using Question = std::variant<YesNoQuestion, ShortAnswerQuestion>;
+std::ostream &operator<<(std::ostream &out, Question q) {
+    std::visit(lager::visitor{
+        [&](auto q) { out << q; },
+    }, q);
+    return out;
+}
 
 struct Nothing {};
 struct Next {};
@@ -41,9 +71,13 @@ struct Model {
 Model init() {
     return {
         Questionnaire{
-            Question{"Are you old enough to drink?"},
+            YesNoQuestion{"Are you old enough to drink?", true},
             {},
-            {{"Do you live in Canada?"}, {"Where do you live?"}}
+            {
+                YesNoQuestion{"Do you live in Canada?"},
+                ShortAnswerQuestion{"Where do you live?", "Calgary"},
+                ShortAnswerQuestion{"What is your name?"}
+            }
         }
     };
 }
@@ -59,19 +93,19 @@ Model update(Model m, Action a) {
 }
 
 void display(Model m) {
-    terminal::clear();
+    //terminal::clear();
     for(auto const &q : m.questions.previousList) {
-        std::cout << green << q.question << std::endl;
+        std::cout << green << q << std::endl;
     }
-    std::cout << white << m.questions.current.question << std::endl;
+    std::cout << white << m.questions.current << std::endl;
     for(auto const &q : m.questions.nextList) {
-        std::cout << grey100 << q.question << std::endl;
+        std::cout << grey100 << q << std::endl;
     }
 }
 
 Action fromChar(char c) {
-    if (c == 'n') return Next{};
-    if (c == 'p') return Prev{};
+    if (c == 'j') return Next{};
+    if (c == 'k') return Prev{};
     return Nothing{};
 }
 
